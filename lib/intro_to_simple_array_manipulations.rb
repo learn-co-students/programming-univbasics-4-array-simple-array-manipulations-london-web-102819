903825c83e8558d8bde1f4ca288a776d8d4f1c7d
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-24 10:29:06 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,65 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow = ["red", "blue", "red"]
+  next_color = "violet"
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(boroughs_in_nyc, new_neighborhood)
+  boroughs_in_nyc = ["Queens", "Manhattan", "brooklyn", "UpperWest"]
+  @new_neighborhood = "Bronx"
+  boroughs_in_nyc.unshift(@new_neighborhood)
+end
+
+def using_pop(continents)
+  continents = ["Europe", "North America", "Africa", "South America", "Asia", "Oceiana", "Antarctica"]
+  continents.pop
+end
+
+def using_pop(continents)
+  continents.pop
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+dog_breeds.pop(2)
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+end
+
+def using_shift(my_favourite_city)
+  my_favourite_city = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  im_so_over_this_city = my_favourite_city.shift
+end
+
+def using_shift(my_favourite_city)
+  my_favourite_city.shift
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  brand_remove = ice_cream_brands.shift(2)
+end
+
+def using_concat(my_favourite_things, more_favs)
+  my_favourite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  all_my_favs = my_favourite_things.concat(more_favs)
+end
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+
